77a4adde99c5481f730197bf7303831e95440699
balibali <bilalnaqvi592@gmail.com>
2025-12-06 17:22:54 +0500

    cors policy set

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: process.env.CLIENT_URI, // frontend port
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json());
 
