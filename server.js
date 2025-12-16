f41823d407eff6e0f9267c7fe9326826ae731b9d
balibali <bilalnaqvi592@gmail.com>
2025-12-16 11:19:32 +0500

    uri add

--- server.js
+++ server.js
@@ -18,17 +18,12 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: [
-      "https://rangrez-events-front-end.vercel.app",
-      "http://localhost:5173",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: process.env.CLIENT_URI, // frontend URI
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"], // Add your required headers here
+}));
 
 app.use(express.json());
 
