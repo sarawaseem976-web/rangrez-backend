1e34f200c7f87becc04306ba75c79ad0b824c8de
balibali <bilalnaqvi592@gmail.com>
2025-12-16 09:26:21 +0500

    uri add

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: process.env.CLIENT_URI, // frontend port
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 }));
 app.use(express.json());
 
